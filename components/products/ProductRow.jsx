c8444516f8a37cae067f3414deea0ab18693bcae
Saad Irfan <105337416+saadirfan370@users.noreply.github.com>
2023-07-19 00:47:14 -0700

    saad

--- components/products/ProductRow.jsx
+++ components/products/ProductRow.jsx
@@ -1,21 +1,31 @@
-import React, { Component } from "react";
-import { FlatList, Text, View } from "react-native";
-import { SIZES } from "../../constants";
+import React from "react";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
+import { COLORS, SIZES } from "../../constants";
 import ProductCardView from "./ProductCardView";
 import styles from "./projuctRow.style";
+import useFetch from "../../hook/usefetch";
 
 const ProductRow = () => {
+  const { data, isLoading, error } = useFetch();
+  
   const product = [1, 2, 3, 4];
   return (
     <View style={styles.container}>
-      <FlatList
-        data={product}
-        renderItem={({ item }) => <ProductCardView />}
-        horizontal
-        contentContainerStyle={{ columnGap: SIZES.medium }}
-      />
+      {isLoading ? (
+        <ActivityIndicator size={SIZES.xLarge} color={COLORS.primary} />
+      ) : error ? (
+        <Text>Something went wrong</Text>
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <ProductCardView />}
+          horizontal
+          contentContainerStyle={{ columnGap: SIZES.medium }}
+        />
+      )}
     </View>
   );
-}; 
+};
 
 export default ProductRow;
---
+++ hook/usefetch.js
@@ -0,0 +1,36 @@
+import React,{ useEffect, useState } from "react";
+import axios from "axios"
+
+const useFetch = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+     const response = await axios.get('http://localhost:3000/api/products')
+     setData(response.data)
+     setIsLoading(false)
+   } catch (err) {
+    setError(err) 
+   }finally{
+    setIsLoading(false)
+   }
+  };
+  console.log(data, isLoading, error,);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error,refetch };
+};
+
+export default useFetch;
